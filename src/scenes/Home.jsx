c34ced2ab295e77dbc398c625b2a89ecbdb7a96d
BlueWebmaster <bluehome88@outlook.com>
2022-05-20 05:27:31 +0900

    drew box

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -22,30 +22,15 @@ import Draw, { createBox } from "ol/interaction/Draw";
 import { OSM, Vector as VectorSource, TileDebug } from "ol/source";
 
 function Home(props) {
-
-  // set intial state
   const [ map, setMap ] = useState()
-  // const [ featuresLayer, setFeaturesLayer ] = useState()
   const [ selectedCoord , setSelectedCoord ] = useState()
-
-  // pull refs
   const mapElement = useRef()
-  
-  // create state ref that can be accessed in OpenLayers onclick callback function
-  //  https://stackoverflow.com/a/60643670
   const mapRef = useRef()
   mapRef.current = map
-
-  // initialize map on first render - logic formerly put into componentDidMount
+  
   useEffect( () => {
-
     const sourceLayer = new VectorSource({ wrapX: false });
-    // create and add vector source layer
-    const initalFeaturesLayer = new VectorLayer({
-      source: sourceLayer
-    })
 
-    // create map
     const initialMap = new Map({
       target: mapElement.current,
       layers: [
@@ -55,7 +40,9 @@ function Home(props) {
         new TileLayer({
           source: new TileDebug()
         }),
-        initalFeaturesLayer
+        new VectorLayer({
+          source: sourceLayer
+        })
       ],
       view: new View({
         minZoom: 3,
@@ -67,50 +54,30 @@ function Home(props) {
       }),
       controls: []
     })
-
-    // set map onclick handler
     initialMap.on('click', handleMapClick)
-
-    // save map and vector layer references to state
-    setMap(initialMap)
-    // setFeaturesLayer(initalFeaturesLayer)
-
     initialMap.addInteraction(new Draw({
       source: sourceLayer,
       type: "Circle",
       geometryFunction: createBox()
     }));
-
+    
+    setMap(initialMap)
   },[])
 
-  // map click handler
   const handleMapClick = (event) => {
-  console.log(event)    
-    // get clicked coordinate using mapRef to access current React state inside OpenLayers callback
-    //  https://stackoverflow.com/a/60643670
     const clickedCoord = mapRef.current.getCoordinateFromPixel(event.pixel);
-
-    // transform coord to EPSG 4326 standard Lat Long
     const transormedCoord = transform(clickedCoord, 'EPSG:3857', 'EPSG:4326')
-
-    // set React state
     setSelectedCoord( transormedCoord )
-    
   }
 
-  // render component
   return (      
     <div className='App'>
-      
       <div ref={mapElement} className="map-container"></div>
-      
       <div className="clicked-coord-label">
         <p>{ (selectedCoord) ? toStringXY(selectedCoord, 5) : '' }</p>
       </div>
-
     </div>
   ) 
-
 }
 
 export default Home
