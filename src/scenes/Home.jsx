1fdc9da3539b1da8c9552528e76f397fdfb02478
BlueWebmaster <bluehome88@outlook.com>
2022-05-23 19:03:36 +0900

    made sea unselectable

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -1,28 +1,22 @@
 import "./style.css"
+import 'ol/ol.css';
 import { useState, useEffect, useRef } from 'react';
+
 import Map from 'ol/Map'
+import Collection from 'ol/Collection';
 import View from 'ol/View'
 import TileLayer from 'ol/layer/Tile'
-import VectorLayer from 'ol/layer/Vector'
-import {toStringXY} from 'ol/coordinate';
+import VectorLayer from 'ol/layer/Vector';
 import Draw, { createBox } from "ol/interaction/Draw";
-import { Snap } from 'ol/interaction';
-import Feature from 'ol/Feature';
-import { Circle as CircleStyle, Fill, Style } from 'ol/style';
-import { Point } from 'ol/geom';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
 import { OSM, Vector as VectorSource } from "ol/source";
+import MVT from 'ol/format/MVT';
 import NftView from "../components/NftView";
 
-// import Web3Context from "../store/web3Context";
-// import Web2Context from "../store/web2Context";
-// import SvgMap from "../components/SvgMap";
-// import { coordToTokenId } from "../utils";
-// import config from "../config.js";
-// import { Button, Typography, message } from "antd";
-
+var lastCoord;
 function Home(props) {
   const [ map, setMap ] = useState()
-  const [ selectedCoord , setSelectedCoord ] = useState()
   const [visible, setVisible] = useState(false)
 
   const mapElement = useRef()
@@ -30,29 +24,15 @@ function Home(props) {
   mapRef.current = map
   
   useEffect( () => {
-    const points = [];
-    let count = 0;
-    for (let i = -20000000; i <= 20000000; i += 200000) {
-      for (let j = -20000000; j <= 20000000; j += 200000) {
-        points[count] = new Feature({
-          'geometry': new Point([i, j])
-        });
-        count++;
-      }
-    }
-    const pointsSource = new VectorSource({
-      features: points,
-      wrapX: false,
+    const landSource = new VectorTileSource({
+      format: new MVT(),
+      url: 'https://api.maptiler.com/tiles/land/{z}/{x}/{y}.pbf?key=oNZwiLnC8cX8YW6850yV',
     });
-    const pointsLayer = new VectorLayer({
-      source: pointsSource,
-      style: new Style({
-        image: new CircleStyle({
-          radius: 1,
-          fill: new Fill({ color: '#ff0000' }),
-        }),
-      })
+    const landLayer = new VectorTileLayer({
+      source: landSource
     });
+    const drawSource = new VectorSource({ wrapX: false });
+    const drawLayer = new VectorLayer({source: drawSource});
 
     const initialMap = new Map({
       target: mapElement.current,
@@ -60,7 +40,8 @@ function Home(props) {
         new TileLayer({
           source: new OSM()
         }),
-        pointsLayer
+        landLayer,
+        drawLayer
       ],
       view: new View({
         minZoom: 3,
@@ -69,42 +50,48 @@ function Home(props) {
         zoom: 3,
         extent: new View().getProjection().getExtent()
       }),
-      controls: []
     })
     setMap(initialMap)
-    initialMap.on('pointermove', handleMapClick)
+    initialMap.on('pointermove', (ev) => {
+      lastCoord = ev.pixel
+    })
 
     const draw = new Draw({
-      source: pointsSource,
+      source: drawSource,
       type: "Circle",
       geometryFunction: createBox()
     })
-    draw.on('drawend', function (ev) {
-      const features = pointsLayer.getFeatures()
-      var lastFeature = features[features.length - 1];
-      pointsLayer.removeFeature(lastFeature);
-      console.log(ev)
-      setVisible(true)
+
+    draw.on('drawstart', function () {
+      drawSource.clear()
+      var flag = false
+      initialMap.forEachFeatureAtPixel(lastCoord, function (feature, layer) {
+        var props = feature.getProperties();
+        if (props.layer === 'land') {
+          flag = true;
+        }
+      })
+      if (!flag)
+        draw.abortDrawing()
+    })
+    drawSource.on('addfeature', function(ev) {
+      var flag = false
+      initialMap.forEachFeatureAtPixel(lastCoord, function (feature, layer) {
+        var props = feature.getProperties();
+        if (props.layer === 'land') {
+          flag = true;
+        }
+      })
+      if (!flag)
+        drawSource.clear()
     })
     initialMap.addInteraction(draw);
-
-    const snap = new Snap({
-      source: pointsSource,
-      pixelTolerance: 100
-    });
-    initialMap.addInteraction(snap);   
   },[])
-
-  const handleMapClick = (event) => {
-    const clickedCoord = mapRef.current.getCoordinateFromPixel(event.pixel);
-    setSelectedCoord(clickedCoord)
-  }
 
   return (
     <div className='App'>
       <div ref={mapElement} className="map-container"></div>
       <div className="clicked-coord-label">
-        <p>{ (selectedCoord) ? toStringXY(selectedCoord, 5) : '' }</p>
         {props.isLogged && visible &&
           <NftView
             tokenId={1}
@@ -124,106 +111,3 @@ function Home(props) {
 }
 
 export default Home
-
-// export default function Home(props) {
-//   const [visible, setVisible] = useState(false);
-//   const [tokenId, setTokenId] = useState(null);
-
-//   const {
-//       initWeb3Modal,
-//       loading,
-//       loadingBuy,
-//       loadingPrice,
-//       loadingCount,
-//       nftPrice,
-//       nftCount,
-//       countLifePixel,
-//       getPixelPrice,
-//       purchasePixel,
-//       openSeaLink,
-//   } = useContext(Web3Context);
-
-//   const {
-//       loadingColors,
-//       nftTokens,
-//       getTokens,
-//   } = useContext(Web2Context);
-
-//   // Get Colors Pixels
-//   useEffect(() => {
-//     if (!nftTokens) {
-//       getTokens();
-//     }
-//   }, [])
-
-//   const handleClickPixel = (coord) => {
-//     const tkId = coordToTokenId(coord.x, coord.y, config.xNum);
-//     setTokenId(tkId);
-//     getPixelPrice(tkId);
-//     countLifePixel(tkId);
-//     setVisible(true);
-//   }
-
-//   const successBuy = async (success) => {
-//     if (success)
-//       message.success('You successfully get this token.');
-//     else
-//       message.error('A error happened. Do you have enough in your balance ?');
-//     setVisible(false);
-
-//     // update grid
-//     getTokens();
-//   }
-
-//   const confirmBuy = (color) => {
-//     purchasePixel(tokenId, color, successBuy);
-//   }
-
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         flexDirection: "column",
-//         width: "100%",
-//         minHeight: '70vh',
-//       }}
-//     >
-//       {props.isLogged && visible && 
-//         <NftView 
-//           tokenId={tokenId}
-//           loading={loadingBuy}
-//           visible={visible}
-//           setVisible={setVisible}
-//           confirmBuy={confirmBuy}
-//           nftPrice={nftPrice}
-//           loadingPrice={loadingPrice}
-//           nftCount={nftCount}
-//           loadingCount={loadingCount}
-//           openSeaLink={openSeaLink}
-//         />}
-
-//       {!loading && (
-//         <>
-//           <SvgMap
-//             width={window.innerWidth}
-//             height={window.innerHeight - 50}
-//             sizeBox={config.sizeBox}
-//             paddBox={config.paddBox}
-//             xNum={config.xNum}
-//             yNum={config.yNum}
-//             onClick={handleClickPixel}
-//             reload={getTokens}
-//             loading={loadingColors}
-//             nftTokens={nftTokens}
-//           />
-//         </>
-//       )}
-      
-//       {loading && (
-//         <Button type="primary" style={{ margin: 'auto' }} loading>
-//           Loading
-//         </Button>
-//       )}
-//     </div>
-//   );
-// }
