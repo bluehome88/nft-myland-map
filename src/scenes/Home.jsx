eb73f0efb35010b0621e16b0e39619e62e871bd1
BlueWebmaster <bluehome88@outlook.com>
2022-05-31 14:11:59 +0900

    show info skeletone done

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -18,153 +18,55 @@ import MVT from 'ol/format/MVT';
 import GeoJSON from 'ol/format/GeoJSON';
 import Stroke from 'ol/style/Stroke';
 import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import Select from 'ol/interaction/Select';
+import { click } from 'ol/events/condition';
 import NftView from "../components/NftView";
-console.log(Photo)
+import { ConsoleSqlOutlined } from "@ant-design/icons";
+import ImageLayer from "ol/layer/Image";
+import Static from "ol/source/ImageStatic";
+
 var lastCoord;
-const geo = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          3.288604,
-          49.719955
-        ]
-      },
-      "properties": {
-        "ref": "APD0004328",
-        "cpost": "02440",
-        "region": "Picardie",
-        "dep": "2",
-        "commune": "Remigny",
-        "text": "Camouflage avec les moyens de fortune",
-        "author": "Opérateur D ; Brissy, Edouard",
-        "date": "1917/06/16",
-        "copy": "Ministère de la Culture (France) - Médiathèque de l'Architecture et du Patrimoine - Diffusion RMN",
-        "img": "http://www.culture.gouv.fr/Wave/image/memoire/1599/sap40_d0004328_v.jpg"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          3.397359,
-          50.328235
-        ]
-      },
-      "properties": {
-        "ref": "APD0005719",
-        "cpost": "59220",
-        "region": "Nord-Pas-de-Calais",
-        "dep": "59",
-        "commune": "Denain",
-        "text": "Visite de la délégation de la Conférence de la paix à l'entrée",
-        "author": "Opérateur D ; Sélince",
-        "date": "1919/04/06",
-        "copy": "Ministère de la Culture (France) - Médiathèque de l'Architecture et du Patrimoine - Diffusion RMN",
-        "img": "http://www.culture.gouv.fr/Wave/image/memoire/1600/sap40_d0005719_v.jpg"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          4.544417,
-          49.184081
-        ]
-      },
-      "properties": {
-        "ref": "APTH002327",
-        "cpost": "51600",
-        "region": "",
-        "dep": "51",
-        "commune": "Souain-Perthes-lès-Hurlus",
-        "text": "Prisonniers allemands au bord de la route",
-        "author": "Opérateur Théta (code armée, photographe)",
-        "date": "1918/10/09",
-        "copy": "Ministère de la Culture (France) - Médiathèque de l'architecture et du patrimoine - diffusion RMN",
-        "img": "http://www.culture.gouv.fr/Wave/image/memoire/1103/sap40_th002327_v.jpg"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          4.730952,
-          49.224849
-        ]
-      },
-      "properties": {
-        "ref": "APTH002364",
-        "cpost": "51800",
-        "region": "",
-        "dep": "51",
-        "commune": "Ripont",
-        "text": "Emplacement du village",
-        "author": "Opérateur Théta (code armée, photographe)",
-        "date": "1918/10/02",
-        "copy": "Ministère de la Culture (France) - Médiathèque de l'architecture et du patrimoine - diffusion RMN",
-        "img": "http://www.culture.gouv.fr/Wave/image/memoire/1103/sap40_th002364_v.jpg"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          3.536178,
-          49.245893
-        ]
-      },
-      "properties": {
-        "ref": "APTH002125",
-        "cpost": "02130",
-        "region": "",
-        "dep": "2",
-        "commune": "Vaux",
-        "text": "Prisonniers allemands faisant la moisson",
-        "author": "Opérateur Théta (code armée, photographe)",
-        "date": "1918/08/24",
-        "copy": "Ministère de la Culture (France) - Médiathèque de l'architecture et du patrimoine - diffusion RMN",
-        "img": "http://www.culture.gouv.fr/Wave/image/memoire/1103/sap40_th002125_v.jpg"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          4.317766,
-          49.298128
-        ]
-      },
-      "properties": {
-        "ref": "APTH002382",
-        "cpost": "51490",
-        "region": "",
-        "dep": "51",
-        "commune": "Pontfaverger",
-        "text": "Ruines",
-        "author": "Opérateur Théta (code armée, photographe)",
-        "date": "1918/09/10",
-        "copy": "Ministère de la Culture (France) - Médiathèque de l'architecture et du patrimoine - diffusion RMN",
-        "img": "http://www.culture.gouv.fr/Wave/image/memoire/1103/sap40_th002382_v.jpg"
-      }
-    }
-  ]
-}
+const geo = [{
+  "coordinates": [
+    30000,
+    434343
+  ],
+  "ref": "APD0004328",
+  "cpost": "02440",
+  "region": "Picardie",
+  "dep": "2",
+  "commune": "Remigny",
+  "text": "Camouflage avec les moyens de fortune",
+  "author": "Opérateur D ; Brissy, Edouard",
+  "date": "1917/06/16",
+  "copy": "Ministère de la Culture (France) - Médiathèque de l'Architecture et du Patrimoine - Diffusion RMN",
+  "img": "http://www.culture.gouv.fr/Wave/image/memoire/1599/sap40_d0004328_v.jpg"
+},
+{
+  "coordinates": [
+    325003.397359,
+    50.328235
+  ],
+  "ref": "APD0005719",
+  "cpost": "59220",
+  "region": "Nord-Pas-de-Calais",
+  "dep": "59",
+  "commune": "Denain",
+  "text": "Visite de la délégation de la Conférence de la paix à l'entrée",
+  "author": "Opérateur D ; Sélince",
+  "date": "1919/04/06",
+  "copy": "Ministère de la Culture (France) - Médiathèque de l'Architecture et du Patrimoine - Diffusion RMN",
+  "img": "http://www.culture.gouv.fr/Wave/image/memoire/1600/sap40_d0005719_v.jpg"
+}]
 function Home(props) {
   const { onePixelCost, buyLands, account } = props
-  const [ map, setMap ] = useState()
+  const [map, setMap] = useState()
   const [visible, setVisible] = useState(false)
   const [rect, setRect] = useState(false)
   const mapElement = useRef()
   const overlayElement = useRef()
+  const infoElement = useRef()
   const price = useRef()
   const myAccount = useRef()
   const buyLandsRef = useRef()
@@ -189,31 +91,29 @@ function Home(props) {
 
   var styleCache = {};
   const getFeatureStyle = (feature, resolution, sel) => {
-    var k = "default_3_" + feature.get("img").match(/[^\\/]+$/)[0] + "_0_0";
-    console.log(k)
-    if (styleCache && styleCache[k]) {
-      return [styleCache[k]];
-    } else {
-      styleCache[k] = new Style({
-        image: new Photo({
+    console.log('********', feature, sel)
+    if (feature.get('img')) {
+      const style = new Style({
+        image: new Icon({
           src: feature.get("img"),
-          radius: 20,
-          // crop: $("#crop").prop('checked'),
-          // kind: $('#kind').val(),
-          // shadow: $("#shadow").prop('checked')?5:0,
-          // onload: function() { vector.changed(); },
-          stroke: new Stroke({
-            width: 3 + (sel ? 3 : 0),
-            color: sel ? 'red' : '#fff'
-          })
+          imgSize: [10, 3]
+          // radius: 1,
+          // crop: true,
+          // stroke: new Stroke({
+          //   width: 3 + (sel ? 3 : 0),
+          //   color: sel ? 'red' : 'blue'
+          // })
         })
       });
+      return [style];
+    } else {
+      return new Style()
     }
   }
 
   if (!window.doMint)
     window.doMint = doMint
-  useEffect( () => {
+  useEffect(() => {
     const landSource = new VectorTileSource({
       format: new MVT(),
       url: 'https://api.maptiler.com/tiles/land/{z}/{x}/{y}.pbf?key=oNZwiLnC8cX8YW6850yV',
@@ -222,37 +122,41 @@ function Home(props) {
       source: landSource
     });
     const drawSource = new VectorSource({ wrapX: false });
-    const drawLayer = new VectorLayer({source: drawSource});
+    const drawLayer = new VectorLayer({ source: drawSource });
     const buyOverlay = new Overlay({
       element: overlayElement.current,
       positioning: "top-left",
       offset: [2, 0]
     })
+    const infoOverlay = new Overlay({
+      element: infoElement.current,
+      positioning: "top-left",
+      offset: [2, 0]
+    })
 
-    const imageSource = new VectorSource({
-      features: new GeoJSON().readFeatures(geo),
-      projection: 'EPSG:3857',
-      attributions: ["&copy; <a href='https://data.culture.gouv.fr/explore/dataset/fonds-de-la-guerre-14-18-extrait-de-la-base-memoire'>data.culture.gouv.fr</a>"],
-      logo: "https://www.data.gouv.fr/s/avatars/37/e56718abd4465985ddde68b33be1ef.jpg"
-    });
-    var imageLayer = new VectorLayer({
-      name: '1914-18',
-      preview: "http://www.culture.gouv.fr/Wave/image/memoire/2445/sap40_z0004141_v.jpg",
-      source: imageSource,
-      // y ordering
-      // renderOrder: ol.ordering.yOrdering(),
-      style: getFeatureStyle
-    });
+    var layers = [
+      new TileLayer({
+        source: new OSM()
+      }),
+      landLayer,
+      drawLayer,
+      // imageLayer
+    ]
+    geo.forEach((i, j) => {
+      const src = new Static({
+        url: i['img'],
+        imageExtent: [i['coordinates'][0], i['coordinates'][1], i['coordinates'][0] + 100000, i['coordinates'][1] + 100000],
+      })
+      const lay = new ImageLayer({
+        source: src,
+        extent: [i['coordinates'][0], i['coordinates'][1], i['coordinates'][0] + 100000, i['coordinates'][1] + 100000]
+      })
+      lay.set('ref', i['ref'])
+      layers.push(lay)
+    })
     const initialMap = new Map({
       target: mapElement.current,
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        }),
-        landLayer,
-        drawLayer,
-        imageLayer
-      ],
+      layers: layers,
       view: new View({
         minZoom: 3,
         maxZoom: 11,
@@ -263,9 +167,20 @@ function Home(props) {
     })
     setMap(initialMap)
     initialMap.addOverlay(buyOverlay)
+    initialMap.addOverlay(infoOverlay)
 
     initialMap.on('click', (ev) => {
-      console.log('click', Date.now())
+      let pixel = initialMap.getEventPixel(ev.originalEvent);
+      let ref = ''
+      initialMap.forEachLayerAtPixel(pixel, (layer, rgb) => {
+        if (layer.get('ref')) {
+          ref = layer.get('ref')
+        }
+      })
+      if (ref) {
+        buyOverlay.setPosition(ev.coordinate)
+        overlayElement.current.innerHTML = ref
+      }
     })
 
     initialMap.on('pointermove', (ev) => {
@@ -295,7 +210,7 @@ function Home(props) {
         draw.abortDrawing()
     })
 
-    drawSource.on('addfeature', function(ev) {
+    drawSource.on('addfeature', function (ev) {
       console.log('addfeature', Date.now())
       var flag = false
       initialMap.forEachFeatureAtPixel(lastCoord, function (feature, layer) {
@@ -329,17 +244,23 @@ function Home(props) {
     draw.on('drawend', function () {
       console.log('drawend', Date.now())
     })
+    // initialMap.addInteraction(draw);
 
-    initialMap.addInteraction(draw);
-  },[])
+    // const select = new Select({
+    //   condition: click,
+    //   style: function (feature, resolution) { console.log('++++++++++', feature); return getFeatureStyle(feature, resolution, true); }
+    // })
+    // initialMap.addInteraction(select);
+  }, [])
 
   return (
     <div className='App'>
       <div ref={mapElement} className="map-container"></div>
       <div ref={overlayElement} className="overlay-container">123123</div>
+      <div ref={infoElement} className="overlay-container">123123</div>
       <div className="clicked-coord-label"></div>
     </div>
-  ) 
+  )
 }
 
 export default Home
