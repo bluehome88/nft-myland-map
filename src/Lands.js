4864d4db8789d18d432cb277d622a8f5e5c731fb
BlueWebmaster <bluehome88@outlook.com>
2022-06-05 02:47:50 +0900

    removed warning

--- src/Lands.js
+++ src/Lands.js
@@ -16,7 +16,7 @@ import {
 import Draw, { createBox } from "ol/interaction/Draw";
 import Overlay from "ol/Overlay";
 import MVT from "ol/format/MVT";
-import {Control, defaults as defaultControls} from 'ol/control';
+import { Control, defaults as defaultControls } from "ol/control";
 import Web3Context from "./store/web3Context";
 import "ol/ol.css";
 import "./lands.css";
@@ -78,42 +78,50 @@ function Lands() {
     }
   };
 
-  class PencilOrHandControl extends Control {
-    constructor(opt_options) {
-      const options = opt_options || {};
-
-      const button = document.createElement('button');
-      button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g fill="white"><path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z"/></g></svg>';
-
-      const element = document.createElement('div');
-      element.className = 'rotate-north ol-unselectable ol-control';
-      element.appendChild(button);
-
-      super({
-        element: element,
-        target: options.target,
-      });
-      button.addEventListener('click', this.handleRotateNorth.bind(this), false);
-      this.status="hand"
-    }
-
-    handleRotateNorth() {
-      const draw = this.getMap().getInteractions().item(this.getMap().getInteractions().getLength()-1)
-      if (this.status=="hand") {
-        this.status = "pencil"
-        draw.setActive(true)
-        this.element.children[0].innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g fill="white"><path d="M480 128v208c0 97.05-78.95 176-176 176h-37.72c-53.42 0-103.7-20.8-141.4-58.58l-113.1-113.1C3.906 332.5 0 322.2 0 312C0 290.7 17.15 272 40 272c10.23 0 20.47 3.906 28.28 11.72L128 343.4V64c0-17.67 14.33-32 32-32s32 14.33 32 32l.0729 176C192.1 248.8 199.2 256 208 256s16.07-7.164 16.07-16L224 32c0-17.67 14.33-32 32-32s32 14.33 32 32l.0484 208c0 8.836 7.111 16 15.95 16S320 248.8 320 240L320 64c0-17.67 14.33-32 32-32s32 14.33 32 32l.0729 176c0 8.836 7.091 16 15.93 16S416 248.8 416 240V128c0-17.67 14.33-32 32-32S480 110.3 480 128z"/></g></svg>';
-      } else {
-        this.status = "hand"
-        draw.setActive(false)
-        this.element.children[0].innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g fill="white"><path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z"/></g></svg>';
-      }
-    }
-  }
-
-
   useEffect(() => {
     if (!window.doMint) window.doMint = doMint;
+
+    class PencilOrHandControl extends Control {
+      constructor(opt_options) {
+        const options = opt_options || {};
+
+        const button = document.createElement("button");
+        button.innerHTML =
+          '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g fill="white"><path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z"/></g></svg>';
+
+        const element = document.createElement("div");
+        element.className = "rotate-north ol-unselectable ol-control";
+        element.appendChild(button);
+
+        super({
+          element: element,
+          target: options.target,
+        });
+        button.addEventListener(
+          "click",
+          this.handleRotateNorth.bind(this),
+          false
+        );
+        this.status = "hand";
+      }
+
+      handleRotateNorth() {
+        const draw = this.getMap()
+          .getInteractions()
+          .item(this.getMap().getInteractions().getLength() - 1);
+        if (this.status === "hand") {
+          this.status = "pencil";
+          draw.setActive(true);
+          this.element.children[0].innerHTML =
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g fill="white"><path d="M480 128v208c0 97.05-78.95 176-176 176h-37.72c-53.42 0-103.7-20.8-141.4-58.58l-113.1-113.1C3.906 332.5 0 322.2 0 312C0 290.7 17.15 272 40 272c10.23 0 20.47 3.906 28.28 11.72L128 343.4V64c0-17.67 14.33-32 32-32s32 14.33 32 32l.0729 176C192.1 248.8 199.2 256 208 256s16.07-7.164 16.07-16L224 32c0-17.67 14.33-32 32-32s32 14.33 32 32l.0484 208c0 8.836 7.111 16 15.95 16S320 248.8 320 240L320 64c0-17.67 14.33-32 32-32s32 14.33 32 32l.0729 176c0 8.836 7.091 16 15.93 16S416 248.8 416 240V128c0-17.67 14.33-32 32-32S480 110.3 480 128z"/></g></svg>';
+        } else {
+          this.status = "hand";
+          draw.setActive(false);
+          this.element.children[0].innerHTML =
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g fill="white"><path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z"/></g></svg>';
+        }
+      }
+    }
 
     /**
      * Initialization of variables
@@ -187,7 +195,6 @@ function Lands() {
       }),
     });
 
-
     /**
      * Add Interactions
      */
@@ -227,22 +234,28 @@ function Lands() {
       drawSource.clear();
       buyOverlay.setPosition(null);
       var flag = false;
-      worldMap.forEachFeatureAtPixel(mouseCoord.current, function (feature, layer) {
-        var props = feature.getProperties();
-        if (props.layer === "land") {
-          flag = true;
+      worldMap.forEachFeatureAtPixel(
+        mouseCoord.current,
+        function (feature, layer) {
+          var props = feature.getProperties();
+          if (props.layer === "land") {
+            flag = true;
+          }
         }
-      });
+      );
       if (!flag) draw.abortDrawing();
     });
     drawSource.on("addfeature", function (ev) {
       var flag = false;
-      worldMap.forEachFeatureAtPixel(mouseCoord.current, function (feature, layer) {
-        var props = feature.getProperties();
-        if (props.layer === "land") {
-          flag = true;
+      worldMap.forEachFeatureAtPixel(
+        mouseCoord.current,
+        function (feature, layer) {
+          var props = feature.getProperties();
+          if (props.layer === "land") {
+            flag = true;
+          }
         }
-      });
+      );
 
       if (!flag) {
         drawSource.clear();
@@ -276,17 +289,14 @@ function Lands() {
         }
       }
     });
-    if ( ! accountRef.current) {
-      draw.setActive(false)
-      const timer = setInterval(
-        () => {
-          if (accountRef.current) {
-            draw.setActive(true)
-            clearInterval(timer)
-          }
-        },
-        1000
-      )
+    if (!accountRef.current) {
+      draw.setActive(false);
+      const timer = setInterval(() => {
+        if (accountRef.current) {
+          draw.setActive(true);
+          clearInterval(timer);
+        }
+      }, 1000);
     }
     worldMap.addInteraction(draw);
   }, []);
